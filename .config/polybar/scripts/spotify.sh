413b0abb6f1441abf41f3060e08b51f127a0435a
Peter Štefunko <xstefunk@fi.muni.cz>
2025-03-24 22:47:58 +0100

    set fixed max size

--- .config/polybar/scripts/spotify.sh
+++ .config/polybar/scripts/spotify.sh
@@ -3,9 +3,11 @@
 data="$HOME/.config/polybar/data"
 poly_right="$data/right.txt"
 
+entries=$(cat "$data/right_entries.txt")
+
 if [[ ! -f $poly_right ]]; then
         touch $poly_right
-        printf '0;0\n%.0s' {1..9} > $poly_right
+        printf '0;0\n%.0s' $(seq 1 "$entries") > "$poly_right"
 fi
 
 status=$(playerctl -p spotify status 2>/dev/null)
